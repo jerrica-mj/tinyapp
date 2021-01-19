090fb65663d5c5a5462a556a933662d98cedecaa
JJ <jerrica.mj@gmail.com>
2021-01-19 06:01:24 +0000

    Prepared for POST route handling: added code to require body-parser library and use to parse request body from Buffer to string.

--- express_server.js
+++ express_server.js
@@ -17,6 +17,15 @@ const urlDatabase = {
 
 
 // ENDPOINT/PATH HANDLING
+// POST REQUESTS
+// require middleware to parse POST request body (buffer) to be readable
+// must come before all route handling, so body-parser library can convert the request body from "Buffer" to readable string, then add the data to the req(request) object under the key "body"
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+// GET REQUESTS
 // handler for the root path, "/"
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,8 +39,8 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// Handle GET (render) and POST (submit) routes for "urls_new" form
-// this must be before "/urls/:shortURL" handler, or express will thisn that "new" is a route parameter--order routes from most to least specific
+// Handle GET (render) route for "urls_new" form
+// this must be before "/urls/:shortURL" handler, or express will think that "new" is a route parameter--order routes from most to least specific
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -44,16 +53,16 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// add additional endpoints/paths
-app.get("/urls.json", (req, res) => {
-  // respond with the urlDatabase object as a JSON string
-  res.json(urlDatabase);
-});
+// // add additional endpoints/paths
+// app.get("/urls.json", (req, res) => {
+//   // respond with the urlDatabase object as a JSON string
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  // send HTML content response
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   // send HTML content response
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 // see if a variable created in one request is accessible in another
 // they are not accessible in other request scopes, and will cause a reference error at that endpoint/url if the endpoint calls a value out of scope
